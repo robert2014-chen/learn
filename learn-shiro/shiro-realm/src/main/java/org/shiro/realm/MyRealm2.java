860f5d120a056b4393272c5fa543550ef0e2da16
robert <qiang421190@163.com>
2017-07-10 14:16:57 +0800

    ''

---
+++ learn-shiro/shiro-realm/src/main/java/org/shiro/realm/MyRealm2.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package org.shiro.realm;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.realm.Realm;
+
+/**
+ * 
+ * @Description
+ * 
+ * @author robert.qiang.chen@gmail.com
+ * 
+ * @time 2017年7月10日 下午2:05:20
+ */
+public class MyRealm2 implements Realm {
+
+	/* (non-Javadoc)
+	 * @see org.apache.shiro.realm.Realm#getName()
+	 */
+	@Override
+	public String getName() {
+		return "a"; //realm name 为 “a”
+	}
+
+	/* (non-Javadoc)
+	 * @see org.apache.shiro.realm.Realm#supports(org.apache.shiro.authc.AuthenticationToken)
+	 */
+	@Override
+	public boolean supports(AuthenticationToken token) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.apache.shiro.realm.Realm#getAuthenticationInfo(org.apache.shiro.authc.AuthenticationToken)
+	 */
+	@Override
+	public AuthenticationInfo getAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+		return new SimpleAuthenticationInfo(
+				"zhang", //身份 字符串类型
+				"123", //凭据
+				getName() //Realm Name
+				);
+	}
+
+}
